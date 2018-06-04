da03483496fd62f0c513c69954dd5ace2d859926
Shen Zhuo-Ran <cmsflash99@gmail.com>
2018-06-04 16:32:20 +0800

    MISC: Remove NoneIO. Make Logger.file non-nonable.

--- beauty/utils/logging.py
+++ beauty/utils/logging.py
@@ -1,28 +1,34 @@
 import os
+import os.path as osp
 import sys
 from io import IOBase
 
 from .osutils import mkdir_if_missing
 
 
-class NoneIO(IOBase):
-    def __init__(self):
-        return
-
-    def read(self):
-        return
-
-    def write(self, *_, **__):
-        return
-    
-
-class Logger(object):
-    def __init__(self, log_path=None):
+class Logger(IOBase):
+    def __init__(self, log_path):
+        super().__init__()
         self.console = sys.stdout
-        self.file = None
-        if log_path is not None:
-            mkdir_if_missing(os.path.dirname(log_path))
-            self.file = open(log_path, 'w')
+        self.file = self._get_file(log_path)
+
+    def _get_file(self, path):
+        mkdir_if_missing(osp.dirname(path))
+        file = open(path, 'w')
+        return file
+
+    def write(self, msg):
+        self.console.write(msg)
+        self.file.write(msg)
+
+    def flush(self):
+        self.console.flush()
+        self.file.flush()
+        os.fsync(self.file.fileno())
+
+    def close(self):
+        self.console.close()
+        self.file.close()
 
     def __del__(self):
         self.close()
@@ -32,19 +38,3 @@ class Logger(object):
 
     def __exit__(self, *args):
         self.close()
-
-    def write(self, msg):
-        self.console.write(msg)
-        if self.file is not None:
-            self.file.write(msg)
-
-    def flush(self):
-        self.console.flush()
-        if self.file is not None:
-            self.file.flush()
-            os.fsync(self.file.fileno())
-
-    def close(self):
-        self.console.close()
-        if self.file is not None:
-            self.file.close()
