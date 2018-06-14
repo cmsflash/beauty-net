123e5c2eb5f93d3abb03df0499737b043101e0dd
Shen Zhuo-Ran <cmsflash99@gmail.com>
2018-06-14 13:51:59 +0800

    TORCH04: Move from deprecated weight initializers.

--- beauty/networks/utils.py
+++ beauty/networks/utils.py
@@ -6,13 +6,13 @@ def init_modules(modules):
     for module in modules:
         if isinstance(module, nn.Conv2d):
             if module.bias is not None:
-                init.uniform(module.bias)
-            init.xavier_uniform(module.weight)
+                init.uniform_(module.bias)
+            init.xavier_uniform_(module.weight)
         if isinstance(module, nn.ConvTranspose2d):
             if module.bias is not None:
-                init.uniform(module.bias)
-            init.xavier_uniform(module.weight)
+                init.uniform_(module.bias)
+            init.xavier_uniform_(module.weight)
         if isinstance(module, nn.Linear):
             if module.bias is not None:
-                init.uniform(module.bias)
-            init.xavier_uniform(module.weight)
+                init.uniform_(module.bias)
+            init.xavier_uniform_(module.weight)
