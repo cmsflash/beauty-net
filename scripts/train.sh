223d4ed2a7477b6f7d043ad07b621a3c9f6a3e47
Shen Zhuo-Ran <cmsflash99@gmail.com>
2018-06-26 12:38:11 +0800

    Update training script.

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-partition=AD_share
+partition=AD4
 gpus=1
 job_name=test
 
 srun -u --partition=${partition} --job-name=${job_name} \
     --gres=gpu:${gpus} -n1 --ntasks-per-node=1 \
     python3 train.py $gpus $job_name
+
