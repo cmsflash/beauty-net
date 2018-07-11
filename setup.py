115817686d1a908dd8a4c17c5273abd8cb662037
Shen Zhuo-Ran <cmsflash99@gmail.com>
2018-07-11 15:21:30 +0800

    Fix email in setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='beauty-net',
     version='0.0.1',
     author='Shen Zhuo-Ran',
-    author_email='cmsflash99@gmai.com',
+    author_email='cmsflash99@gmail.com',
     install_requires=['torch', 'torchvision'],
     packages=find_packages()
 )
