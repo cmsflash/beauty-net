9b1a1437c118f15fe28243a14f79b1d27372e478
Shen Zhuo-Ran <cmsflash99@gmail.com>
2018-07-11 15:22:42 +0800

    beauty-net 0.0.2 releases.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='beauty-net',
-    version='0.0.1',
+    version='0.0.2',
     author='Shen Zhuo-Ran',
     author_email='cmsflash99@gmail.com',
     install_requires=['torch', 'torchvision'],
