55964826bdbb87d50b3d8d70c23ca44a33e4f328
Shen Zhuo-Ran <cmsflash99@gmail.com>
2018-07-12 16:38:42 +0800

    Move model_trainer into beauty.

--- beauty/model_trainer.py
+++ beauty/model_trainer.py
@@ -1,6 +1,6 @@
-from beauty import networks, metrics, lr_schedulers, data_loaders
-from beauty.model_runners import Trainer, Evaluator
-from beauty.utils import tensor_utils, serialization
+from . import networks, metrics, lr_schedulers, data_loaders
+from .model_runners import Trainer, Evaluator
+from .utils import tensor_utils, serialization
 
 
 class ModelTrainer:
--- train.py
+++ train.py
@@ -4,9 +4,8 @@ import sys
 
 from torch import nn, optim
 
-from beauty import networks, metrics, lr_schedulers, datasets
+from beauty import ModelTrainer, networks, metrics, lr_schedulers, datasets
 from beauty.networks.beauty_net import BeautyNet
-from model_trainer import ModelTrainer
 
 
 if __name__ == '__main__':
