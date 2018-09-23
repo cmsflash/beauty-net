ce9f392b62ba5410b4a95f85525888f5ad8d79a8
Shen Zhuoran <cmsflash99@gmail.com>
2018-09-23 15:29:16 +0800

    UTILS: Rename dir to dir_ in os_utils.

--- beauty/utils/os_utils.py
+++ beauty/utils/os_utils.py
@@ -3,9 +3,9 @@ from os import path as osp
 import shutil
 
 
-def make_dir_if_missing(dir):
-    if not osp.exists(dir):
-        os.makedirs(dir)
+def make_dir_if_missing(dir_):
+    if not osp.exists(dir_):
+        os.makedirs(dir_)
 
 
 def remove_if_exists(path):
