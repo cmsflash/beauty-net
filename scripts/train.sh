74f5eaa32f99efb389c9fc4ae2c75be2df94a1d2
Shen Zhuoran <cmsflash99@gmail.com>
2018-09-23 17:20:36 +0800

    Paraterize train.sh with job_name.

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-partition=AD1
-gpus=1
-job_name=test
+partition=AD2
+gpus=8
+job_name=$1
 
-srun -u --partition=${partition} --job-name=${job_name} \
-    --gres=gpu:${gpus} -n1 --ntasks-per-node=1 \
-    python3 train.py $gpus $job_name
+log_dir=logs/$job_name
+rm $log_dir -rf
+mkdir -p $log_dir
 
+srun -u --partition=$partition --job-name=$job_name \
+    --gres=gpu:$gpus -n1 --ntasks-per-node=1 \
+    python3 $job_name.py $gpus $job_name \
+    | tee $log_dir/log.txt
