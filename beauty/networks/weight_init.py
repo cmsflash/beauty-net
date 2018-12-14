a6e2a1dccae6f82d620ae8e0b7f6de1d3f7c161b
Shen Zhuoran <cmsflash99@gmail.com>
2018-12-14 14:53:14 +0800

    TORCH10: Adopt nn.init.ones_() and .zeros_()

--- beauty/networks/weight_init.py
+++ beauty/networks/weight_init.py
@@ -6,11 +6,12 @@ def init(modules):
         if isinstance(module, nn.Conv2d):
             nn.init.xavier_normal_(module.weight)
             if module.bias is not None:
-                nn.init.constant_(module.bias, 0)
+                nn.init.zeros_(module.bias)
         elif isinstance(module, nn.BatchNorm2d):
-            nn.init.constant_(module.weight, 1)
-            nn.init.constant_(module.bias, 0)
+            nn.init.ones_(module.weight)
+            nn.init.zeros_(module.bias)
         elif isinstance(module, nn.Linear):
             nn.init.xavier_uniform_(module.weight)
             if module.bias is not None:
                 nn.init.uniform_(module.bias)
+
